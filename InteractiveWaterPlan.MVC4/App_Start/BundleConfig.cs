82ddc768287736f498c39fac0a3dea8983f0b78c
James Seppi <james.seppi@gmail.com>
2013-01-15 15:37:54 -0600

    removed stupidtable

--- InteractiveWaterPlan.MVC4/App_Start/BundleConfig.cs
+++ InteractiveWaterPlan.MVC4/App_Start/BundleConfig.cs
@@ -17,7 +17,6 @@ namespace InteractiveWaterPlan.MVC4
                 "~/Scripts/jquery.number.js",
                 "~/Scripts/OpenLayers/tile.stamen.js",
                 "~/Scripts/Compiled/OpenLayers/*.js",
-                "~/Scripts/stupidtable.js",
                 "~/Scripts/jquery.dataTables.js",
                 "~/Scripts/DT_bootstrap.js",
                 "~/Scripts/chosen.jquery.js",
