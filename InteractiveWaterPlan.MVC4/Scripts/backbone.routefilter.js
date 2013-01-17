139b27994600b36b032aa18018e28f47f3671106
James Seppi <james.seppi@gmail.com>
2013-01-17 15:19:38 -0600

    updated to work with new backbone version

--- InteractiveWaterPlan.MVC4/Scripts/backbone.routefilter.js
+++ InteractiveWaterPlan.MVC4/Scripts/backbone.routefilter.js
@@ -1,4 +1,5 @@
 //From: https://github.com/danharper/backbone_filters
+//Updated by James Seppi to work with Backbone 0.9.10
 (function() {
     _.extend(Backbone.Router.prototype, Backbone.Events, {
         before: {},
@@ -23,14 +24,15 @@
             return failingFilter ? false : true;
         },
         route: function(route, name, callback) {
-            Backbone.history || (Backbone.history = new Backbone.History);
             if (!_.isRegExp(route)) route = this._routeToRegExp(route);
+            if (!callback) callback = this[name];
             Backbone.history.route(route, _.bind(function(fragment) {
-                var args = this._extractParameters(route, fragment);
+                var args = this._extractParameters(route, fragment);    
                 if (this._runFilters(this.before, fragment, args)) {
-                    callback.apply(this, args);
+                    callback && callback.apply(this, args);
                     this._runFilters(this.after, fragment, args);
                     this.trigger.apply(this, ['route:' + name].concat(args));
+                    Backbone.history.trigger('route', this, name, args);
                 }
             }, this));
         }
