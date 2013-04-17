b3faea298c05b77f9b2d23c41add71afa7c66120
James Seppi <james.seppi@gmail.com>
2013-04-17 10:17:09 -0500

    typo fix

--- InteractiveWaterPlan.MVC4/Scripts/build-iswp-app.bat
+++ InteractiveWaterPlan.MVC4/Scripts/build-iswp-app.bat
@@ -5,5 +5,5 @@ cd %1\Scripts\Compiled\backbone_app
 REM Build the non-optimized app, output to Scripts folder
 call r.js.cmd -o optimize=none baseUrl=. name=main.js out=../../iswp-app.js paths.scripts=../.. paths.templates=../../templates
 
-REM Build the optimized version, output to Scrips folder
+REM Build the optimized version, output to Scripts folder
 call r.js.cmd -o baseUrl=. name=main.js out=../../iswp-app.min.js paths.scripts=../.. paths.templates=../../templates
