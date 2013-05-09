158e8211481ab975e454b5d2db4f50a1cbc40434
James Seppi <james.seppi@gmail.com>
2013-05-09 10:15:47 -0500

    logging exceptions caught by filter to Elmah

--- InteractiveWaterPlan.MVC4/Filters/GenericExceptionFilter.cs
+++ InteractiveWaterPlan.MVC4/Filters/GenericExceptionFilter.cs
@@ -9,7 +9,10 @@ namespace InteractiveWaterPlan.MVC4.Filters
     {
         public override void OnException(HttpActionExecutedContext context)
         {
-            string message = "Sorry, an error has occurred.";
+            //TODO: replace with generic exception mesage
+            string message = context.Exception.Message;
+
+            // If it is an argument exception, let the user know
             if (context.Exception is ArgumentException){
                 message = context.Exception.Message;
             }
@@ -17,9 +20,10 @@ namespace InteractiveWaterPlan.MVC4.Filters
             context.Response = context.Request.CreateErrorResponse(
                 HttpStatusCode.BadRequest, message);
 
+            // Lastly, have Elmah log the exception
+            Elmah.ErrorSignal.FromCurrentContext().Raise(context.Exception);
+
             base.OnException(context);
         }
-
-  
     }
 }
