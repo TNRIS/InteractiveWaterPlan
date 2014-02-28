9f0bf6b20a46a29ce8acbb44a06d088026e03588
James Seppi <james.seppi@gmail.com>
2014-02-28 13:57:56 -0600

    starting express/angular project

---
+++ v1/app/scripts/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular.module('v1App', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute'
+])
+  .config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'partials/main',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+      
+    $locationProvider.html5Mode(true);
+  });
