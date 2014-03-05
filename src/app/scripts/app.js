e44c41301a5820f2b2fdf0dec9f9600f7cc691c4
James Seppi <james.seppi@gmail.com>
2014-03-05 16:49:44 -0600

    year and theme select placeholders

--- src/app/scripts/app.js
+++ src/app/scripts/app.js
@@ -4,7 +4,8 @@ angular.module('iswpApp', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
-  'ngRoute'
+  'ngRoute',
+  'ui.bootstrap'
 ])
   .config(function ($routeProvider, $locationProvider) {
     $routeProvider
