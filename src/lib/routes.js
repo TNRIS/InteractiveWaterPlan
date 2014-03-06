f05b326410dc8db131c9debdefa04159432a8821
James Seppi <james.seppi@gmail.com>
2014-03-06 16:07:29 -0600

    refactor index; add about placeholder

--- src/lib/routes.js
+++ src/lib/routes.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var api = require('./controllers/api'),
+about = require('./controllers/about'),
     index = require('./controllers');
 
 /**
@@ -9,14 +10,18 @@ var api = require('./controllers/api'),
 module.exports = function(app) {
 
   // Server API Routes
-  app.get('/api/awesomeThings', api.awesomeThings);
-  
+  var apiPre = '/api/v1/';
+  app.get(apiPre + 'awesomeThings', api.awesomeThings);
+  app.get(apiPre + 'regions', api.regions);
+  app.get(apiPre + 'counties', api.counties);
 
   // All undefined api routes should return a 404
   app.get('/api/*', function(req, res) {
     res.send(404);
   });
-  
+
+  app.get('/about', about.about);
+
   // All other routes to use Angular routing in app/scripts/app.js
   app.get('/partials/*', index.partials);
   app.get('/*', index.index);
