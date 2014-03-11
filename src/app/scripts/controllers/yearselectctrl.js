2fcca96139c69b1236f002ea3de616047d23891f
James Seppi <james.seppi@gmail.com>
2014-03-10 23:27:12 -0500

    add changeroute factory

--- src/app/scripts/controllers/yearselectctrl.js
+++ src/app/scripts/controllers/yearselectctrl.js
@@ -1,12 +1,13 @@
 'use strict';
 
 angular.module('iswpApp')
-  .controller('YearSelectCtrl', function ($scope, $routeParams) {
+  .controller('YearSelectCtrl', function ($scope, $routeParams, $changeRoute) {
     $scope.years = ['2010', '2020', '2030', '2040', '2050'];
 
     $scope.selectedYear = $routeParams.year;
 
     $scope.selectYear = function(year) {
       console.log("YearSelectCtrl select year", year);
+      $changeRoute({year: year});
     };
   });
