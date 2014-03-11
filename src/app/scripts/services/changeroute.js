d1bb17af81086fa7af32927a4778db23dead77bf
James Seppi <james.seppi@gmail.com>
2014-03-10 23:27:24 -0500

    add changeroute factory

---
+++ src/app/scripts/services/changeroute.js
@@ -0,0 +1,23 @@
+'use strict';
+
+angular.module('iswpApp')
+  .factory('$changeRoute', function $changeRoute($routeParams, $route, $location) {
+    return function(params) {
+      params = angular.extend({}, $routeParams, params);
+        
+      var callback = function(all, name) {
+        if (params[name]) {
+          return params[name];
+        }
+        return '';
+      };
+      
+      for (var path in $route.routes) {
+        if ($route.routes[path] === $route.current.$$route) {
+          var url = path.replace(/:(\w+\??)/g, callback);
+          $location.path(url);
+          return;
+        }
+      }
+    };
+  });
