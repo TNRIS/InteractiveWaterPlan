b283d8ff43b78877e0a00bef5559c794e2b914a3
James Seppi <james.seppi@gmail.com>
2014-03-12 13:59:44 -0500

    a little refactoring

--- src/app/scripts/controllers/needs/needsSummaryTableCtrl.js
+++ src/app/scripts/controllers/needs/needsSummaryTableCtrl.js
@@ -3,31 +3,29 @@
 angular.module('iswpApp')
   .controller('NeedsSummaryTableCtrl', function ($scope, needsData) {
 
+    $scope.tableColumns = [
+      {map: 'REGION', label: 'Region'},
+      {map: 'MUNICIPAL', label: 'Municipal', formatFunction: 'number'},
+      {map: 'MANUFACTURING', label: 'Manufacturing', formatFunction: 'number'},
+      {map: 'MINING', label: 'Mining', formatFunction: 'number'},
+      {map: 'STEAMELECTRIC', label: 'Steam-Electric', formatFunction: 'number'},
+      {map: 'LIVESTOCK', label: 'Livestock', formatFunction: 'number'},
+      {map: 'IRRIGATION', label: 'Irrigation', formatFunction: 'number'},
+      {map: 'TOTAL', label: 'Total', formatFunction: 'number'},
+    ];
+
+    $scope.tableConfig = {
+      isPaginationEnabled: false
+    };
+
     $scope.$on('$stateChangeSuccess', function() {
       $scope.currentYear = $scope.$stateParams.year;
 
-      //TODO: This is only valid for summary table. generalize or make different controllers.
+      //Get only the needsData for the currentYear
       var dataForYear = _.where(needsData, {DECADE:
         $scope.currentYear});
 
       $scope.tableRows = dataForYear;
-
-      $scope.tableColumns = [
-        {map: 'REGION', label: 'Region'},
-        {map: 'MUNICIPAL', label: 'Municipal', formatFunction: 'number'},
-        {map: 'MANUFACTURING', label: 'Manufacturing', formatFunction: 'number'},
-        {map: 'MINING', label: 'Mining', formatFunction: 'number'},
-        {map: 'STEAMELECTRIC', label: 'Steam-Electric', formatFunction: 'number'},
-        {map: 'LIVESTOCK', label: 'Livestock', formatFunction: 'number'},
-        {map: 'IRRIGATION', label: 'Irrigation', formatFunction: 'number'},
-        {map: 'TOTAL', label: 'Total', formatFunction: 'number'},
-      ];
-
-      $scope.tableConfig = {
-        isPaginationEnabled: false
-      };
-      //--end TODO
-
     });
     return;
   });
