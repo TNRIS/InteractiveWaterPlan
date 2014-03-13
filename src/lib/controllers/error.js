3e50dfc6ebb21569ca7210e99f91af84a3824f2a
James Seppi <james.seppi@gmail.com>
2014-03-13 16:15:55 -0500

    respond based on req accept type

--- src/lib/controllers/error.js
+++ src/lib/controllers/error.js
@@ -6,5 +6,18 @@
  */
 exports.index = function(req, res) {
   res.status(404);
-  res.render('404');
+
+  if (req.accepts('html')) {
+    res.render('404');
+    return;
+  }
+
+  // respond with json
+  if (req.accepts('json')) {
+    res.send({ error: 'Not found' });
+    return;
+  }
+
+  // default to plain-text. send()
+  res.type('txt').send('Not found');
 };
