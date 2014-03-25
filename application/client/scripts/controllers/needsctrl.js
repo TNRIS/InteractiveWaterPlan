e1ee3d8c38459a40ab654be81079f107d312fc0e
James Seppi <james.seppi@gmail.com>
2014-03-25 09:15:24 -0500

    setup validation stuff

--- application/client/scripts/controllers/needsctrl.js
+++ application/client/scripts/controllers/needsctrl.js
@@ -3,31 +3,8 @@
 angular.module('iswpApp')
   .controller('NeedsCtrl',
     function ($scope, $rootScope, ISWP_VARS) {
-
-      //TODO: route validation (maybe this should be done in states.js?)
-      //See: http://angular-ui.github.io/ui-router/site/#/api/ui.router.router.$urlRouter
-
-
-      // var validateRouteParams = function(evt, toState, toParams, fromState, fromParams) {
-      //   console.log("validateRouteParams")
-
-      //   var validAreas = ['state']
-      //     .concat(ISWP_VARS.regions)
-      //     .concat(ISWP_VARS.counties);
-
-      //   // if (!_.contains(validAreas, $routeParams.area)) {
-      //   //   evt.preventDefault();
-      //   // }
-
-      //   if (!_.contains(ISWP_VARS.years, toParams.year)) {
-      //     evt.preventDefault();
-      //   }
-      //   //TODO: validate $routeParams.subtheme
-      // };
-
-      // //Validate routeParams, redirect when invalid
-      // $scope.$on('$stateChangeStart', validateRouteParams);
-
+      //Does nothing?
+      //TODO: remove if so
       return;
     }
   );
--- application/client/scripts/states.js
+++ application/client/scripts/states.js
@@ -1,76 +1,104 @@
 'use strict';
 
-angular.module('iswpApp').config(
-  function($stateProvider, $urlRouterProvider, $locationProvider, $uiViewScrollProvider) {
-    //don't use html5 urls because lack of support in IE (so sharing URLs will not work well)
-    $locationProvider.html5Mode(false);
+angular.module('iswpApp')
+  .run(function($rootScope, $state, $location, ISWP_VARS) {
+    $rootScope.$on('$stateChangeStart', function(evt, toState, toParams) {
 
-    //disable whacky auto-scroll behavior
-    $uiViewScrollProvider.useAnchorScroll();
-
-    //redirect any bad/unmapped route to the beginning
-    $urlRouterProvider
-      .otherwise('/needs/2010/state');
-
-
-    var needsResolver = function(type, typeIdProperty) {
-      return {
-        needsData: function(NeedsService, $stateParams) {
-          return NeedsService.fetch(type, $stateParams[typeIdProperty]);
-        }
+      var doesntContain = function() {
+        return !_.contains.apply(null, arguments);
       };
-    };
+
+      if (doesntContain(ISWP_VARS.years, toParams.year)) {
+        $location.url('/');
+        return;
+      }
+
+      switch (toState.name) {
+        case 'needs.region':
+
+          break;
+
+        case 'needs.county':
+          break;
+
+        case 'needs.type':
+          break;
+      }
+
+    });
+  })
+  .config(function($stateProvider, $urlRouterProvider, $locationProvider,
+    $uiViewScrollProvider, $anchorScrollProvider) {
+      //don't use html5 urls because lack of support in IE (so sharing URLs will not work well)
+      $locationProvider.html5Mode(false);
+
+      //disable whacky auto-scroll behavior
+      $uiViewScrollProvider.useAnchorScroll();
+      $anchorScrollProvider.disableAutoScrolling();
+
+      //redirect any bad/unmapped route to the beginning
+      $urlRouterProvider.otherwise('/needs/2010/state');
 
 
-    $stateProvider
-      .state('needs', {
-        abstract: true,
-        url: '/needs',
-        resolve: {
-          entities: function(EntityService) {
-            return EntityService.fetch();
-          }
-        },
-        templateUrl: 'partials/needs/needs_index.html',
-        controller: 'NeedsCtrl'
-      })
-      .state('needs.summary', {
-        url: '/:year/state', // appended to /needs
-        resolve: needsResolver('summary'),
-        controller: 'NeedsSummaryTableCtrl',
-        templateUrl: 'partials/needs/needs_table.html'
-      })
-      .state('needs.region', {
-        url: '/:year/region/:region', // appended to /needs
-        resolve: needsResolver('region', 'region'),
-        controller: 'NeedsRegionTableCtrl',
-        templateUrl: 'partials/needs/needs_table.html'
-      })
-      .state('needs.county', {
-        url: '/:year/county/:county', // appended to /needs
-        resolve: needsResolver('county', 'county'),
-        controller: 'NeedsCountyTableCtrl',
-        templateUrl: 'partials/needs/needs_table.html'
-      })
-      .state('needs.type', {
-        url: '/:year/type/:entityType', // appended to /needs
-        resolve: needsResolver('type', 'entityType'),
-        controller: 'NeedsEntityTypeTableCtrl',
-        templateUrl: 'partials/needs/needs_table.html'
-      })
-      .state('needs.entity', {
-        url: '/:year/entity/:entityId', // appended to /needs
-        resolve: {
+      var needsResolver = function(type, typeIdProperty) {
+        return {
           needsData: function(NeedsService, $stateParams) {
-            return NeedsService.fetch('entity', $stateParams.entityId);
-          },
-          entitySummary: function(EntityService, $stateParams) {
-            return EntityService.fetchSummary($stateParams.entityId);
+            return NeedsService.fetch(type, $stateParams[typeIdProperty]);
           }
-        },
-        controller: 'NeedsEntityTableCtrl',
-        templateUrl: 'partials/needs/needs_entity_view.html'
-      });
+        };
+      };
 
-      //TODO demands, supplies, wms (later phases)
-  });
+
+      $stateProvider
+        .state('needs', {
+          abstract: true,
+          url: '/needs',
+          resolve: {
+            entities: function(EntityService) {
+              return EntityService.fetch();
+            }
+          },
+          templateUrl: 'partials/needs/needs_index.html',
+          controller: 'NeedsCtrl'
+        })
+        .state('needs.summary', {
+          url: '/:year/state', // appended to /needs
+          resolve: needsResolver('summary'),
+          controller: 'NeedsSummaryTableCtrl',
+          templateUrl: 'partials/needs/needs_table.html'
+        })
+        .state('needs.region', {
+          url: '/:year/region/:region', // appended to /needs
+          resolve: needsResolver('region', 'region'),
+          controller: 'NeedsRegionTableCtrl',
+          templateUrl: 'partials/needs/needs_table.html'
+        })
+        .state('needs.county', {
+          url: '/:year/county/:county', // appended to /needs
+          resolve: needsResolver('county', 'county'),
+          controller: 'NeedsCountyTableCtrl',
+          templateUrl: 'partials/needs/needs_table.html'
+        })
+        .state('needs.type', {
+          url: '/:year/type/:entityType', // appended to /needs
+          resolve: needsResolver('type', 'entityType'),
+          controller: 'NeedsEntityTypeTableCtrl',
+          templateUrl: 'partials/needs/needs_table.html'
+        })
+        .state('needs.entity', {
+          url: '/:year/entity/:entityId', // appended to /needs
+          resolve: {
+            needsData: function(NeedsService, $stateParams) {
+              return NeedsService.fetch('entity', $stateParams.entityId);
+            },
+            entitySummary: function(EntityService, $stateParams) {
+              return EntityService.fetchSummary($stateParams.entityId);
+            }
+          },
+          controller: 'NeedsEntityTableCtrl',
+          templateUrl: 'partials/needs/needs_entity_view.html'
+        });
+
+        //TODO demands, supplies, wms (later phases)
+    }
+  );
