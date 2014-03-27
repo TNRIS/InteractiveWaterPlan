e4ab641f8894b841f568dc16d196bf055a91b239
James Seppi <james.seppi@gmail.com>
2014-03-27 08:37:58 -0500

    change label to 'Existing Water Supply', fix #56

--- application/client/scripts/controllers/needs/needsEntityTableCtrl.js
+++ application/client/scripts/controllers/needs/needsEntityTableCtrl.js
@@ -74,7 +74,7 @@ angular.module('iswpApp')
           data: [
             ['Category', 'Amount (acre-ft/year)', {role: 'style'}, {role: 'tooltip'}],
             ['Water Demand', demand, '#666', demand.format() + ' acre-feet/year'],
-            ['Water Supply', supply, '#007fff', supply.format() + ' acre-feet/year'],
+            ['Existing Water Supply', supply, '#007fff', supply.format() + ' acre-feet/year'],
             ['Water Need', needs, '#aa0000', needs.format() + ' acre-feet/year'],
             ['Strategy Supply',strategySupply, '#ff7518', strategySupply.format() + ' acre-feet/year'],
           ]
