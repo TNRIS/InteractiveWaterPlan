0e7c80985d3dd7ce3baef82dbfe2d8823924ff7a
wilsaj <wilson.andrew.j+github@gmail.com>
2014-03-28 09:00:13 -0500

    use subdomains for tile layers to get better request throughput

--- application/client/scripts/config.js
+++ application/client/scripts/config.js
@@ -3,7 +3,7 @@
 angular.module('iswpApp.config', [])
   .constant('BING_API_KEY', 'Ar54FaSONDkvSeqhwoBnBW61JYlThqD8XVtwlaRAcUZDfKQzDjo2kjkMLKT3LCVi')
   .constant('SWP_WMS_URL', 'http://services.tnris.org/ags/services/swp/swp/MapServer/WMSServer')
-  .constant('TILES_URL', 'http://216.178.233.219/tiles')
+  .constant('TILES_URL', 'http://tile{s}.texasstatewaterplan.org/tiles')
   .constant('ISWP_VARS', ISWP_VARS) //boostrapped variables
   .constant('API_PATH', '/api/v1/')
 ;
