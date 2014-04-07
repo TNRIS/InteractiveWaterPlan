54ac697b6ca1d1f9e9dfa49581a2d99c48ade7bb
James Seppi <james.seppi@gmail.com>
2014-04-07 10:52:23 -0500

    remove x-powered-by header

--- application/server/config/express.js
+++ application/server/config/express.js
@@ -50,6 +50,7 @@ module.exports = function(app) {
   app.configure(function(){
     app.engine('html', require('ejs').renderFile);
     app.set('view engine', 'html');
+    app.disable('x-powered-by');
     app.use(express.logger('dev'));
     app.use(express.bodyParser());
     app.use(express.methodOverride());
