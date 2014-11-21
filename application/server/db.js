5fcf7fc4324295cd4abf9d4e1c2fbadea1c738ca
James Seppi <james.seppi@gmail.com>
2014-11-20 18:00:48 -0600

    fix export of db

--- application/server/db.js
+++ application/server/db.js
@@ -5,4 +5,13 @@ var config = require('./config/config');
 
 var db = new sqlite3.Database(config.dbPath, sqlite3.OPEN_READONLY);
 
+var knex = require('knex')({
+  client: 'sqlite3',
+  connection: {
+    filename: config.dbPath
+  }
+});
+
+db.knex = knex;
 module.exports = exports = db;
+// exports.knex = knex;
