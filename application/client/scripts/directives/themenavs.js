cba9a7d3ce0ed448ba350911820092c83a4d4c6e
James Seppi <james.seppi@gmail.com>
2014-11-26 11:20:13 -0600

    styling upgrade for theme navs

--- application/client/scripts/directives/themenavs.js
+++ application/client/scripts/directives/themenavs.js
@@ -1,27 +1,14 @@
 'use strict';
 
 angular.module('iswpApp')
-  .directive('themeNavs', function($state, $stateParams) {
+  .directive('themeNavs', function ($state, $stateParams) {
     return {
       restrict: 'A',
       replace: true,
-      template: ['<div>',
-          '<ul class="nav nav-pills theme-nav">',
-            '<li ng-class="{active: parentState == \'demands\'}">',
-              '<a ng-href="{{demandsRef}}" title="Demands">Demands</a>',
-            '</li>',
-            '<li ng-class="{active: parentState == \'needs\'}">',
-              '<a ng-href="{{needsRef}}" title="Needs">Needs</a>',
-            '</li>',
-            '<li ng-class="{active: parentState == \'strategies\'}">',
-              '<a ng-href="{{strategiesRef}}" title="Recommended Strategy Supplies">Strategy Supplies</a>',
-            '</li>',
-          '</ul>',
-        '</div>'].join(''),
+      templateUrl: 'templates/themenavs.html',
       link: function postLink(scope, element, attrs) {
 
-        scope.$on('$stateChangeSuccess', function() {
-
+        function updateNavLinks() {
           var splitState = $state.current.name.split('.');
           scope.parentState = _.first(splitState);
           var childStateName = _.last(splitState);
@@ -29,9 +16,11 @@ angular.module('iswpApp')
           scope.demandsRef = $state.href('demands.' + childStateName, $stateParams);
           scope.needsRef = $state.href('needs.' + childStateName, $stateParams);
           scope.strategiesRef = $state.href('strategies.' + childStateName, $stateParams);
-        });
+        }
 
+        updateNavLinks();
 
+        scope.$on('$stateChangeSuccess', updateNavLinks);
       }
     };
   });
