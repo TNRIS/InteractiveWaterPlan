adb75d4e67666edd7d842c353849be488271cda3
James Seppi <james.seppi@gmail.com>
2014-12-02 17:40:47 -0600

    make isChildState filter

--- application/client/scripts/filters/filters.js
+++ application/client/scripts/filters/filters.js
@@ -5,4 +5,12 @@ angular.module('iswpApp')
     return function(input) {
       return angular.isString(input) ? input.titleize() : '';
     };
-  });
+  })
+  //Like ui-router's isState, but checks if is child state
+  .filter('isChildState', function($state) {
+    return function(state) {
+      var parent = $state.current.name.split('.')[0];
+      return $state.is(parent + '.' + state);
+    };
+  })
+  ;
