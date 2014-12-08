c6771f7de4d1def3cccb76d559aad92fbdddff96
James Seppi <james.seppi@gmail.com>
2014-12-08 11:38:34 -0600

    use new id field, remove debug stuff

--- application/server/controllers/api/sources.js
+++ application/server/controllers/api/sources.js
@@ -6,7 +6,7 @@ var db = require('./../../db');
 var validators = require('./../../lib/validators');
 
 function selectSources() {
-  return db.select('sourceId', 'type', 'geojson', 'mappingPoint')
+  return db.select('id', 'type', 'geojson', 'mappingPoint')
     .from('SourceFeatures');
 }
 
@@ -18,7 +18,7 @@ function toSourceFeatureCollection(sourceResults) {
 
   var gjs = sourceResults.map(function (sourceResult) {
     var gj = JSON.parse(sourceResult.geojson);
-    gj.properties.sourceId = sourceResult.sourceId;
+    gj.properties.sourceId = sourceResult.id;
     gj.properties.sourceType = sourceResult.type;
     return gj;
   });
@@ -32,7 +32,7 @@ exports.getSourcesById = function getSourcesById(req, res) {
   var sourceId = req.params.sourceId;
 
   selectSources()
-    .where('sourceId', sourceId)
+    .where('id', sourceId)
     .then(function (result) {
       var fc = toSourceFeatureCollection(result);
       if (!fc) {
@@ -58,7 +58,7 @@ exports.getSourcesByIds = function getSourcesByIds(req, res) {
   sourceIds = toIntArr(sourceIds);
 
   selectSources()
-    .whereIn('sourceId', sourceIds)
+    .whereIn('id', sourceIds)
     .then(function (result) {
       var fc = toSourceFeatureCollection(result);
       if (!fc) {
@@ -69,21 +69,8 @@ exports.getSourcesByIds = function getSourcesByIds(req, res) {
     });
 };
 
-exports.getSourceMappingPoints = function getSourceMappingPoints(req, res) {
-  var sourceIds = req.query.ids;
-
-  sourceIds = toIntArr(sourceIds);
-
-  return db.select('sourceId', 'mappingPoint', 'geojson')
-    .from('SourceFeatures')
-    .whereIn('sourceId', sourceIds)
-    .then(function (result) {
-      res.json(result);
-    });
-};
 
 var router = express.Router();
-router.get('/points', validators.validateSourceIds, exports.getSourceMappingPoints);
 router.get('/:sourceId', validators.validateSourceId, exports.getSourcesById);
 router.get('/', validators.validateSourceIds, exports.getSourcesByIds);
 exports.router = router;
