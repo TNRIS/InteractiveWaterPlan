2dcecc7158502c3f7a7237c224d765bebcaad002
James Seppi <james.seppi@gmail.com>
2014-12-11 09:26:08 -0600

    add periods

--- application/client/scripts/controllers/strategies/strategiesSummaryCtrl.js
+++ application/client/scripts/controllers/strategies/strategiesSummaryCtrl.js
@@ -4,8 +4,8 @@ angular.module('iswpApp').controller('StrategiesSummaryCtrl',
   function ($scope, strategiesData, TreeMapFactory, SUMMARY_TABLE_COLS, ISWP_VARS, API_PATH) {
 
     $scope.heading = 'Regional Summary of Recommended Water Management Supply Strategies';
-    $scope.mapDescription = 'Map shows Regional Water Planning Areas that may be selected using cursor';
-    $scope.tableDescription = 'Table summarizes recommended strategy supplies by region and water use category in acre-feet/year (click on region for summary)';
+    $scope.mapDescription = 'Map shows Regional Water Planning Areas that may be selected using cursor.';
+    $scope.tableDescription = 'Table summarizes recommended strategy supplies by region and water use category in acre-feet/year (click on region for summary).';
 
     $scope.downloadPath = API_PATH + 'strategies/summary?format=csv';
 
